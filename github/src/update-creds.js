cf9e9bff1ef1242e2ae54f87f9de04dd0a5f153d
Mark Koelle <mkoelle@gmail.com>
2022-01-10 22:37:53 -0600

    create creds once, not in loop

--- github/src/update-creds.js
+++ github/src/update-creds.js
@@ -6,18 +6,18 @@ const github = require("./github");
 const main = async () => {
   const aws_repos = await github.getReposWithTopic('mkoelle', 'aws');
   const env = await aws.getAccountEnv()
+  const account = await aws.getAccountId()
+  const accessKey = await aws.generateNewAccessKey()
   Promise.all(
     aws_repos.map(async repo => {
       console.log(`updating ${env} deployment credentials for ${repo.name}`)
       await github.createEnvironment(repo.name, env);
-      const account = await aws.getAccountId()
-      const accessKey = await aws.generateNewAccessKey()
       await github.putEnvironmentSecret(repo.id, env, "AWS_ACCOUNT", account);
       await github.putEnvironmentSecret(repo.id, env, "AWS_ACCESS_KEY_ID", accessKey.AccessKeyId);
       await github.putEnvironmentSecret(repo.id, env, "AWS_SECRET_ACCESS_KEY", accessKey.SecretAccessKey);
-      aws.deleteOldAccessKey(accessKey.AccessKeyId)
     })
   )
+  aws.deleteOldAccessKey(accessKey.AccessKeyId)
 }
 
 main()
